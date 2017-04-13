fd114b585dab6150e1d748f5af588bee730c4345
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2017-04-14 01:52:20 +0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@
 
 var fs = require('fs');
 var gulp = require('gulp');
+var deploy = require('gulp-gh-pages');
 
 /**
  *  This will load all js or coffee files in the gulp directory
@@ -26,4 +27,9 @@ fs.readdirSync('./gulp').filter(function(file) {
  */
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
+});
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
 });
